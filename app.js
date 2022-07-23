f31f5e4ee047cc9948a97fcd4f6120c5ffb8bcf4
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-07-23 17:55:35 +0500

    Conmplete without input

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-let timeLeft = document.querySelector(`#time_left`);
-let birthday = new Date(document.querySelector(`#bDate`))
-let today = new Date()
+const timeLeft = document.getElementById("time-left");
 
-console.log(today);
+//month day year
+const birthday = new Date('07/24/2022');
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+let timerId;
+
+function countDown() {
+  const today = new Date();
+  const timeSpan = birthday - today;
+  //milliseconds
+  // console.log(timeSpan);
+
+  if (timeSpan <= -day) {
+    timeLeft.innerHTML = "Hope you had a nice Birthday!!";
+    clearInterval(timerId);
+    return;
+  }
+
+  else if (timeSpan <= 0) {
+    timeLeft.innerHTML = "Happy Birthday!!";
+    clearInterval(timerId);
+    return;
+  }
+
+  const days = Math.floor(timeSpan / day);
+  const hours = Math.floor((timeSpan % day) / hour);
+  const minutes = Math.floor((timeSpan % hour) / minute);
+  const seconds = Math.floor((timeSpan % minute) / second);
+
+  timeLeft.innerHTML =
+    days + "days " + hours + "hrs " + minutes + "mins " + seconds + "secs";
+}
+
+timerId = setInterval(countDown, second);
